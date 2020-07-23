2d997db34f0ef6757ea9a96ce1f4108297dff3c4
Henrique Leite <henriqueleite616@gmail.com>
2020-07-23 20:01:17 -0300

    Updates

---
+++ precache-manifest.c6d43cf3bd07648ec263f1bb828bee51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2dccb6bce8e59dab0a210b856d35aa6",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "37e32872b16493ceaae2",
+    "url": "/kanban/static/css/2.d75e0ccb.chunk.css"
+  },
+  {
+    "revision": "773237e4cd6acb61fe39",
+    "url": "/kanban/static/css/main.b8e7111e.chunk.css"
+  },
+  {
+    "revision": "37e32872b16493ceaae2",
+    "url": "/kanban/static/js/2.8375b382.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kanban/static/js/2.8375b382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773237e4cd6acb61fe39",
+    "url": "/kanban/static/js/main.47eef18e.chunk.js"
+  },
+  {
+    "revision": "b0d6c60d14b3a7b407ce",
+    "url": "/kanban/static/js/runtime-main.8ce35ea3.js"
+  }
+]);
